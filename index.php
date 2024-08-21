b62541965b8fd566e8cb7614fa5d63ab6fd25f0c
Jose Daniel Ferrufino Barahona <daniferrufino1234@gmail.com>
2024-08-20 22:21:41 -0600

    Daniel
    yo

--- index.php
+++ index.php
@@ -1,2 +1,21 @@
-<?php
+<!Doctype html>
+<html>
+    <head>
+        <title>Jose Daniel Ferrufino Barahona</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css" /> 
+        <link rel="stylesheet" href="css/style.css" />
+        <script src="js/jquery-3.7.1.min.js"></script>
+        <script src="js/bootstrap.bundle.min.js"></script>
 
+    </head>
+    <body>
+
+
+    </body>
+    <fooder>
+
+
+    </fooder>
+
+
+</html>
